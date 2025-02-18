4db2440e3e87aa4c2a22759e94e6c9e700a06ae2
David Spreekmeester <david@spreekmeester.nl>
2025-02-18 20:56:39 +0100

    Fix path

--- cron-run.sh
+++ cron-run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-cd /share/CACHEDEV1_DATA/Docker/rtsp-timelapse-docker && \
+cd /share/CACHEDEV1_DATA/Docker/rtsp-timelapse && \
 docker compose exec app /usr/local/webcam/snap.sh && \
 cd -
