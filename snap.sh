03568ebedb94e517ec01322146619181bdef7693
David Spreekmeester <david@spreekmeester.nl>
2025-02-18 21:01:18 +0100

    Fix orphan container issue

--- snap.sh
+++ snap.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker compose run app bash -c "sed -i '32 s/\&//' snap.sh && chmod +x snap.sh && ./snap.sh"
+docker compose run --rm app bash -c "sed -i '32 s/\&//' snap.sh && chmod +x snap.sh && ./snap.sh"
