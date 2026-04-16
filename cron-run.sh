ef83e805d1c142da904c52a7ccc224f12b201049
David Spreekmeester <david@spreekmeester.nl>
2026-04-16 14:18:31 +0200

    Docker pruning after one-shot run, no more container restarts

--- cron-run.sh
+++ cron-run.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
-cd /share/CACHEDEV1_DATA/Docker/rtsp-timelapse && \
-/share/CACHEDEV1_DATA/.qpkg/container-station/bin/docker compose run --rm app bash -c "sed -i '32 s/\&//' snap.sh && chmod +x snap.sh && ./snap.sh" && \
-cd -
+DOCKER=/share/CACHEDEV1_DATA/.qpkg/container-station/bin/docker
+cd /share/CACHEDEV1_DATA/Docker/rtsp-timelapse && $DOCKER compose run --rm --name timelapse-snap app bash -c "sed -i '32 s/\&//' snap.sh && chmod +x snap.sh && ./snap.sh" ; $DOCKER container prune -f > /dev/null 2>&1
